644cfe04a2cf37c67f7a6c59de090b361b5a0ee5
akhtwe <akhtwe@global-connect.asia>
2020-09-18 17:14:57 +0630

    initial setup file

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Find your rooms | Make your properties</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+</head>
+<body>
+    <?php include('navbar.php'); ?>
+</body>
+<script src="/js/jquery-3.5.1.slim.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/popper.min.js"></script>
+</html>
