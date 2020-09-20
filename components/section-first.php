f65380b5ce040a2d421a6a20fae5a6dc5ccb4fb4
akhtwe <akhtwe@global-connect.asia>
2020-09-20 23:33:31 +0630

    Slick and Layout

---
+++ components/section-first.php
@@ -0,0 +1,56 @@
+<div class="bg-img">
+    <section class=" bg-opacity ">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 pt-5 pl-5">
+                <div class=" rounded shadow-lg p-4  search-section-first">
+                    <div class="text-section-first">
+                       <b> FIND  HOTELS, RESORTS, MOTELS & MORE</b>
+                        <p> Get the best prices on 2,000,000+ properties</p>
+                    </div>
+                <div class="wrapper pb-4"> 
+                    <input type="text" 
+                            class="text form-control input-height"
+                            placeholder="Enter Username"
+                            name="uname" required>  
+                    <img src="/images/icons/bed.png" alt=""> 
+                </div> 
+                <div class="wrapper pb-4"> 
+                    <input type="text" 
+                            id="daterange"
+                            class="text form-control input-height"
+                            placeholder="Check in date   |   Check out date"
+                            name="checkin-checkout" required>  
+                    <img src="/images/icons/calendar.png" alt=""> 
+                </div> 
+                <div class="row">
+                    <div class="col-md-8">
+                    <div class="wrapper"> 
+                    <input type="text" 
+                            class="text form-control input-height"
+                            placeholder="2 Adults  |  1 Room"
+                            name="checkin-checkout" required>  
+                    <img src="/images/icons/group.png" alt=""> 
+                </div> 
+                    </div>
+                    <div class="col-md-4">
+                        <button type="submit" class="btN form-control input-height text-white bg-puple"> <b class="roboto">SEARCH</b></button>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="listing-section-first rounded py-5 text-center align-middle">
+                    <div class="pt-5">
+                        <h1 class="playfair"><b class="text-white">Make Your </b><b class="text-warning">End-To-End</b><b class="text-white"> Property</b></h1>
+                        <h4 class="text-white playfair py-4"> Help the guests to easily find out more your properties.</h4>
+                        <div>
+                            <a href="#" class="text-decoration-none roboto"><b class="text-warning" style="font-size:20px;"> Get Free Listing </b> &emsp;<img src="/images/icons/right-arrow.png" alt=""></a>
+                        </div>
+                        </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+</section>
+</div>
--- index.php
+++ index.php
@@ -5,11 +5,134 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Find your rooms | Make your properties</title>
     <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/first-section.css">
+    <link rel="stylesheet" href="/css/rmm.css">
+    <link rel="stylesheet" href="/css/slick.css">
+    <!-- <link rel="stylesheet" href="/css/slick-theme.css"> -->
+
+<link rel="stylesheet" type="text/css" href="/css/daterangepicker.css" />
+    <style>
+    @font-face {
+        font-family: 'Playfair Display', serif;
+        font-family: 'Roboto', sans-serif;
+        src: url('/fonts/Rototo/*') format('embedded-opentype');
+        src: url('/fonts/Playfair/*');
+    }
+    .roboto{
+        font-family: 'Roboto', sans-serif;
+    }
+    .playfair{
+        font-family: 'Playfair Display', serif;
+    }
+    </style>
 </head>
 <body>
-    <?php include('navbar.php'); ?>
+<section class="bg-light">
+<?php include('components/navbar.php');?>
+<?php include('components/section-first.php'); ?>
+<?php include('components/section-second.php');?>
+<div class="container">
+    <div class="row py-4">
+        <div class="col-3 px-0">
+            <div class="bg-white border rounded p-3  text-center  pb-5 shadow">
+                <h3 class="text-center append-border">
+                    <b class="roboto ">Today's detals</b>
+                    <hr class="border-half">
+                    <br>
+                </h3>
+              
+                <p class="text-justify py-4 playfair">
+                   <i> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat culpa in dolorum aut labore dolore expedita.</i>
+                </p>
+                <a href="#" class="text-decoration-none"><h3><b class="text-primary" style="color:#6177EB;">See All </b><img src="/images/icons/right-arrow-blue.png" alt=""></h3></a>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <?php include('components/section-deals.php') ?>
+        </div>
+    </div>
+</div>
+</section>
+   
 </body>
 <script src="/js/jquery-3.5.1.slim.min.js"></script>
 <script src="/js/bootstrap.min.js"></script>
 <script src="/js/popper.min.js"></script>
+<script src="/js/fontawesome.js"></script>
+<script src="/js/slick.js"></script>
+
+<script type="text/javascript" src="/js/moment.min.js"></script>
+<script type="text/javascript" src="/js/daterangepicker.min.js"></script>
+<script>
+    $(document).ready(function(){
+        var date = new Date();
+        $('#daterange').daterangepicker({
+            autoUpdateInput: false,
+            minDate:moment(date).add(0,'days'),
+            locale: {
+                    cancelLabel: 'Clear'
+                    }
+        })
+        .on('apply.daterangepicker hide.daterangepicker', function(ev, picker) {
+                var start = moment(picker.startDate);
+                var end   = moment(picker.endDate);
+                var diff =end.diff(start, 'days'); 
+                $(this).val(picker.startDate.format('ddd, DD MMM') + '  |  ' + picker.endDate.format('ddd, DD MMM')+'   ('+(diff+1)+'-days,'+(diff)+'-nights)');
+               
+                
+        }).on('cancel.daterangepicker', function(ev, picker) {
+                $(this).val('');
+            });
+
+        $('.deal-slick').slick({
+            autoplay:true,
+            autoplaySpeed: 2000,
+
+            slidesToShow: 3,
+            slidesToScroll:1,
+            arrows:true,
+            nextArrow: $('#custom-next-slick'),
+            prevArrow: $('#custom-prev-slick'),
+            dots: false,
+
+            responsive: [{
+                breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                         infinite: true
+                         }
+                        },{
+                breakpoint: 600,
+                    settings: {
+                    slidesToShow: 2,
+                    dots: false
+                    }
+                    },{ breakpoint: 400,
+                    settings: {
+                    slidesToShow: 1,
+                    dots: false
+                    }
+                    },
+                ]
+        });
+        $('.deal-slick').on('afterChange', function(event, slick, currentSlide, nextSlide){
+            var start=currentSlide+1;
+            var end=start+2;
+            end=end>slick.slideCount?(end-slick.slideCount):end;
+            $('#deail-slider-count').html(start+"-"+end +" rooms / "+slick.slideCount+" rooms");
+        });
+        $('#custom-next-slick').click(function(){
+            $('.deal-slick').prev().slick('slickNext');
+            });
+        $('#custom-prev-slick').click(function(){
+            $('.deal-slick').prev().slick('slickPrev');
+            });
+        $('#custom-prev-slick-slide').click(function(){
+
+        });
+        $('#custom-next-slick-slide').click(function(){
+           
+        });
+    });
+</script>
 </html>
